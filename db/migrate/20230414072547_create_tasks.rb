0daaf9978872319242560f822169e64b057fe0ce
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2023-04-17 10:03:10 +0300

    Database schema

---
+++ db/migrate/20230414072547_create_tasks.rb
@@ -0,0 +1,12 @@
+class CreateTasks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :tasks do |t|
+      t.string :name
+      t.text :description
+      t.boolean :completed
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
